657f900c86e913ab0d3e4c280d7503e839c9840c
Sina Maleki <sina67631@gmail.com>
2024-05-07 00:23:17 +0330

    added my own context manager

--- core/db.py
+++ core/db.py
@@ -10,3 +10,13 @@ class DataBase:
 
     def __getitem__(self, item):
         return self.__store[item]
+
+    def load(self):
+        pass
+
+    def save(self):
+        pass
+
+    def register(self, model):
+        self.__class__.__models[model.__name__] = model
+        self.__class__.__store.setdefault(model.__name__, model.store)
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import atexit
+
 from core.db import DataBase
+from models import User
 
 if __name__ == "__main__":
     db = DataBase("db.pickle")
-    db.register()
+    db.register(User)
+
+    atexit.register(db.save)
