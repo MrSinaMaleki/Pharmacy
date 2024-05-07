76b613453aedc9ef052bc493d915e1c133a533cf
Sina Maleki <sina67631@gmail.com>
2024-05-07 10:56:52 +0330

    completed the db file for dealing with our database

--- core/db.py
+++ core/db.py
@@ -1,9 +1,10 @@
 import pickle
+from .filehandler import Editor
 
 
 class DataBase:
     __store = {}
-    __models = set()
+    __models = {}
 
     def __init__(self, path: str) -> None:
         self.path = path
@@ -12,10 +13,21 @@ class DataBase:
         return self.__store[item]
 
     def load(self):
-        pass
+        try:
+            with Editor(self.path, "rb") as file:
+                self.__store = pickle.load(file)
+
+                for model in self.__models.values():
+                    model.store = self.__store.get(model.__name__, [])
+        except (FileNotFoundError, EOFError):
+            pass
 
     def save(self):
-        pass
+        with Editor(self.path, "wb") as file:
+            for model in self.__models.values():
+                self.__store[model.__name__] = model.store
+
+            pickle.dump(self.__store, file)
 
     def register(self, model):
         self.__class__.__models[model.__name__] = model
