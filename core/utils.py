a1d674d5f89f70d58f21a4dc4de9e39c06d98e04
Sina Maleki <sina67631@gmail.com>
2024-05-09 09:47:41 +0330

    added router and admin file

--- core/utils.py
+++ core/utils.py
@@ -8,3 +8,19 @@ def clear():
 def banner(title: str):
     clear()
     print("_" * 40, f"\u001b[38;5;4m{title.title().center(40)}\u001b[0m", "=" * 40, sep="\n")
+
+
+def safe(callback):
+    def _(route):
+        try:
+            callback(route)
+        except Exception as e:
+            banner(" Error ❗ ")
+            print(f"\u001b[38;5;1m{str(e)}\u001b[0m")
+            if (cmd := input("\nTry Again ? [Y|n] ").strip().lower()) and cmd[0] == "n":
+                route.parent()
+            else:
+                banner(" ⚜️ " + route.name + " ⚜️ ")
+                callback(route)
+
+    return _
